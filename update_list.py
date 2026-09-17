d80f57901a3ee1158714c9fb96890848ac9ce80e
SUYASH-27-UI <suyashsurve30@gmail.com>
2026-09-17 12:01:22 +0530

    Create update_list.py

---
+++ update_list.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50]
+
+print("Original list:", numbers)
+
+position = int(input("Enter index to change: "))
+new_value = int(input("Enter new value: "))
+
+numbers[position] = new_value
+
+print("Updated list:", numbers)
